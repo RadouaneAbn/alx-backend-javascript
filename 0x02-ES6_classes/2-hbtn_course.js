555c7f12531b13ddece129d769e68a970b32d894
RadouaneAbn <radouaneabn@gmail.com>
2024-05-30 23:40:40 +0100

    Task 2 - check in the setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -26,14 +26,19 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') { throw (new Error('Name must be a string')); }
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') { throw (new Error('Length must be a number')); }
     this._length = length;
   }
 
   set students(students) {
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      throw (new Error('Students must be an array of strings'));
+    }
     this._students = students;
   }
 }
