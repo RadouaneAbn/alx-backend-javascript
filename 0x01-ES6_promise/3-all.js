26b9b0df8c7065350e8ae72792cce6867b350e20
RadouaneAbn <radouaneabn@gmail.com>
2024-05-30 01:59:33 +0100

    Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    return { status: 200, body: 'success' };
+  } catch (error) {
+    return Error();
+  }
+}
