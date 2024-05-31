c9e010d939a9182de8a5fa970245a87e64144b8c
RadouaneAbn <radouaneabn@gmail.com>
2024-05-31 01:03:14 +0100

    Task 5 - cleaning

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -9,9 +9,4 @@ export default class Building {
   get sqft() { return this._sqft; }
 
   set sqft(sqft) { this._sqft = sqft; }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
