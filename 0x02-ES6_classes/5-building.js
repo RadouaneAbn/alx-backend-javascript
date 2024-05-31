bac5ca39e2e899524db66b77e855c8a3805ec2eb
RadouaneAbn <radouaneabn@gmail.com>
2024-05-31 01:13:13 +0100

    Task 5 - change methode

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') { throw new Error('sqft must be a number'); }
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
+    if (typeof sqft !== 'number') { throw new Error('sqft must be a number'); }
     this._sqft = sqft;
   }
 
