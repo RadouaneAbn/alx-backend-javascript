138eadd9a3c90ac81b654de774c81a96579769e8
RadouaneAbn <radouaneabn@gmail.com>
2024-06-05 02:35:58 +0100

    Task 9 - use forEach

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -6,6 +6,6 @@ export default function groceriesList() {
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
-  ].map((item) => newMap.set(...item));
+  ].forEach((item) => newMap.set(...item));
   return newMap;
 }
